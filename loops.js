93b5134c1f7ffdec357a8a34d12d329257d4a4a9
damianoyo06 <nowakowd@tcd.ie>
2024-09-26 12:18:22 +0100

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,46 @@
+
+let names = ["Damian", "David", "John", "Steward", "Milot", "Anna", "Abdul"];
+const myName = "Damian"
+
+let x = 0;
+
+
+let hour;
+let minutes;
+let greet;
+let randomName;
+
+
+hour = Math.floor(Math.random() * 24);
+minutes = Math.floor(Math.random() * 59);
+randomName = Math.floor(Math.random() * 6);
+
+
+
+// for(x in names){
+//     if(names[x] == "Damian")
+//     {
+//         console.log("This is your name")
+//     }
+//     else{
+//         console.log("The name is " + names[x])
+//     }
+// }
+
+if(hour < 12){
+    greet = "Good morning, ";
+}
+else if(hour <18){
+    greet = "Good afternoon, ";
+}
+else{
+    greet = "Good evening, ";
+}
+
+function tellTime(){
+    console.log(greet + names[randomName] + " it is currently " + hour + ":" + minutes)
+}
+
+tellTime();
+
+
